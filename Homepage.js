10042ce066afd80cef898b05d346a6db0764fb58
Keening <simkeemin@gmail.com>
2026-08-14 16:09:27 +0800

    last changes w syntax plus comments

--- Homepage.js
+++ Homepage.js
@@ -1,143 +1,133 @@
-async function generateRead() {
+function generateRead() {
 
     const book = document.querySelector(".book-generated");
     const card = document.querySelector(".card-bg");
 
+//Books found in website
 
- let books = [
-    {
-        "title": "Miss Forensics",
-        "image": "Images/miss-forensics-1.webp",
-        "synopsis": "A forensic expert and a sharp detective investigate difficult cases while gradually building a close bond.",
+    let books = [
+        {
+            "title": "Miss Forensics",
+            "image": "Images/MissForensicsCover.webp",
+            "synopsis": "A forensic expert and a sharp detective investigate difficult cases while gradually building a close bond.",
+            "type": "Manhua",
+            "tags": ["Drama", "Mystery", "Thriller"]
+        },
+
+        {
+            "title": "My Food Looks Very Cute!",
+            "image": "Images/MyFoodLooksVeryCuteTodayCover.webp",
+            "synopsis": "A wandering werewolf accidentally awakens a vampire and the two begin travelling together after becoming connected by a mysterious curse.",
+            "type": "Manhua",
+            "tags": ["Slice-Of-Life", "Supernatural", "Fluff"]
+        },
+
+        {
+            "title": "Sora & Haena",
+            "image": "Images/Sora&HaenaCover.png",
+            "synopsis": "Shy Sora and outgoing Haena make a deal to help each other with romance and college, only for their relationship to grow closer.",
+            "type": "Manhwa",
+            "tags": ["Comedy", "Drama", "School Life"]
+        },
+
+        {
+            "title": "The Love Doctor",
+            "image": "Images/TheLoveDoctorCover.webp",
+            "synopsis": "Jung Erae seeks romantic advice from love doctor Cha Yoon but unexpectedly begins developing feelings for her instead.",
+            "type": "Manhwa",
+            "tags": ["Comedy", "Drama", "Slice-Of-Life"]
+        },
+
+        {
+            "title": "Can't Defy The Lonely Girl",
+            "image": "Images/ICan'tSayNoToTheLonelyGirlCover.jpg",
+            "synopsis": "Ayaka agrees to help delinquent Sora return to school in exchange for a recommendation, but Sora asks for something unexpected in return.",
+            "type": "Manga",
+            "tags": ["Drama", "School Life", "Slice-Of-Life"]
+        },
+
+        {
+            "title": "Yuri is Forbidden for Yuri Ota?!",
+            "image": "Images/YuriIsATabooSubjectForAYuriOtakuCover.webp",
+            "synopsis": "Yuri fan Fuyu enters an all-girls school hoping to observe perfect romances but finds herself unexpectedly drawn into one with energetic Rika.",
+            "type": "Manga",
+            "tags": ["Comedy", "Drama", "School Life"]
+        },
+
+        {
+            "title": "The Summer You Were There",
+            "image": "Images/TheSummerYouWereThereCover.jpg",
+            "synopsis": "Reclusive writer Shizuku is approached by Kaori, who suggests they date so Shizuku can use their relationship as inspiration for her next story.",
+            "type": "Manga",
+            "tags": ["Drama", "Tragedy", "Slice-Of-Life", "Medical", "Psychological"]
+        },
+
+        {
+        "title": "Tamen De Gushi",
+        "image": "Images/TamenDeGushiCover.jpg",
+        "synopsis": "Sun Jing and Qiu Tong attend different high schools, and their friendship slowly develops into something more.",
         "type": "Manhua",
-        "tags": ["Drama", "Mystery", "Thriller"]
-    },
+        "tags": ["Fluff", "Slice-Of-Life", "Highschool"]
+        },
 
-    {
-        "title": "My Food Looks Very Cute!",
-        "image": "Images/My food looks very cute today.webp",
-        "synopsis": "A wandering werewolf accidentally awakens a vampire and the two begin travelling together after becoming connected by a mysterious curse.",
+        {
+        "title": "Clear and Muddy Loss of Love",
+        "image": "Images/Clear&MuddyLossOfLoveCover.jpeg",
+        "synopsis": "After losing her homeland in war, a former prince enters the enemy kingdom seeking revenge but is unexpectedly appointed as the princess's spouse.",
         "type": "Manhua",
-        "tags": ["Slice-Of-Life", "Supernatural", "Fluff"]
-    },
+        "tags": ["Drama", "Historical", "Action"]
+        }
+    ]
 
-    {
-        "title": "Sora & Haena",
-        "image": "Images/Sora&Haena.png",
-        "synopsis": "Shy Sora and outgoing Haena make a deal to help each other with romance and college, only for their relationship to grow closer.",
-        "type": "Manhwa",
-        "tags": ["Comedy", "Drama", "School Life"]
-    },
+    //Randomly selects a book from book list
 
-    {
-        "title": "The Love Doctor",
-        "image": "Images/the-love-doctor.webp",
-        "synopsis": "Jung Erae seeks romantic advice from love doctor Cha Yoon but unexpectedly begins developing feelings for her instead.",
-        "type": "Manhwa",
-        "tags": ["Comedy", "Drama", "Slice-Of-Life"]
-    },
+    const randomIndex = Math.floor(Math.random() * books.length);
+    const randomBook = books[randomIndex];
 
-    {
-        "title": "Can't Defy The Lonely Girl",
-        "image": "Images/I_Can't_Say_No_to_the_Lonely_Girl_1_Japanese_Cover.jpg",
-        "synopsis": "Ayaka agrees to help delinquent Sora return to school in exchange for a recommendation, but Sora asks for something unexpected in return.",
-        "type": "Manga",
-        "tags": ["Drama", "School Life", "Slice-Of-Life"]
-    },
+    //Changes image and alt text
+    book.src = randomBook.image;
+    book.alt = randomBook.title + "Book Cover";
 
-    {
-        "title": "Yuri is Forbidden for Yuri Ota?!",
-        "image": "Images/yuri-is-taboo-to-a-yuri-otaku-v0-t7if2wq4280c1.webp",
-        "synopsis": "Yuri fan Fuyu enters an all-girls school hoping to observe perfect romances but finds herself unexpectedly drawn into one with energetic Rika.",
-        "type": "Manga",
-        "tags": ["Comedy", "Drama", "School Life"]
-    },
+    //Resets book generator
+    book.classList.remove("generated");
+    book.classList.remove("enhanced");
+    void book.offsetWidth;
 
-    {
-        "title": "The Summer You Were There",
-        "image": "Images/Thesummeryouwerethere.jpg",
-        "synopsis": "Reclusive writer Shizuku is approached by Kaori, who suggests they date so Shizuku can use their relationship as inspiration for her next story.",
-        "type": "Manga",
-        "tags": ["Drama", "Tragedy", "Slice-Of-Life", "Medical", "Psychological"]
-    },
+    //Book appearance
+    book.classList.add("generated");
+    book.style.display = "block";
 
-    {
-    "title": "Tamen De Gushi",
-    "image": "Images/tamende gushi.jpg",
-    "synopsis": "Sun Jing and Qiu Tong attend different high schools, and their friendship slowly develops into something more.",
-    "type": "Manhua",
-    "tags": ["Fluff", "Slice-Of-Life", "Highschool"]
-    },
-
-    {
-    "title": "Clear and Muddy Loss of Love",
-    "image": "Images/Clear-and-Muddy-Loss-of-Love.jpeg",
-    "synopsis": "After losing her homeland in war, a former prince enters the enemy kingdom seeking revenge but is unexpectedly appointed as the princess's spouse.",
-    "type": "Manhua",
-    "tags": ["Drama", "Historical", "Action"]
-    }
-]
-
-
-
-        const randomIndex = Math.floor(Math.random() * books.length);
-
-        const randomBook = books[randomIndex];
-
-     
-        book.src = randomBook.image;
-        book.alt = randomBook.title;
-
-      
-        book.classList.remove("generated");
-        book.classList.remove("enhanced");
-
-        void book.offsetWidth;
-
-        book.classList.add("generated");
-
-        book.style.display = "block";
-
-        const colour = ["lightsteelblue", "lightblue", "lightgreen"]
-        
+    //Color of tags
+    const colour = ["lightsteelblue", "lightblue", "lightgreen"]
       
 
-            const title = card.querySelector("h2");
-            const synopsis = card.querySelector(".synopsis");
-            const type = card.querySelector(".type");
-            const tags = card.querySelector(".tags");
+    const title = card.querySelector("h2");
+    const synopsis = card.querySelector(".synopsis");
+    const type = card.querySelector(".type");
+    const tags = card.querySelector(".tags");
+    
+    //changes content of html elements of pop-up card
+    title.textContent = randomBook.title;
+    synopsis.innerHTML = "<b>" + "Synopsis: " + "</b>" + randomBook.synopsis
+    type.innerHTML = "<b>" + "Type: " + "</b>" + randomBook.type
+    tags.innerHTML = "";
 
+    //Creating genre tags
+    randomBook.tags.forEach(function(tag) {
 
-            title.textContent = randomBook.title;
+        const li = document.createElement("li");
+        const randomColor = Math.floor(Math.random() * colour.length);
 
+        li.style.backgroundColor = colour[randomColor];
+        li.style.borderColor = colour[randomColor]
+        li.textContent = tag;
 
-
-            synopsis.innerHTML = "<b>" + "Synopsis: " + "</b>" + randomBook.synopsis
-        
-
-            
-            type.innerHTML = "<b>" + "Type: " + "</b>" + randomBook.type
-
-            tags.innerHTML = "";
-
-            randomBook.tags.forEach(function(tag) {
-
-                    const li = document.createElement("li");
-                    const randomColor = Math.floor(Math.random() * colour.length);
-
-                    li.style.backgroundColor = colour[randomColor];
-                    li.style.borderColor = colour[randomColor]
-                    li.textContent = tag;
-
-                    tags.appendChild(li);
+        tags.appendChild(li);
 
                     
 
-                });
-            }
-
-
-  
+    });
+}
 
 
 function enhance() {
@@ -146,6 +136,7 @@ function enhance() {
 
     const isOpen = book.classList.contains("enhanced");
 
+    //Enhancing and returning card to normal size
     if (isOpen) {
         book.classList.remove("enhanced");
         card.style.display = "none";
@@ -167,11 +158,14 @@ function changeSlide() {
 
     currentIndex++;
 
+    //Resting Carousel
     if (currentIndex >= slides.length) {
         currentIndex = 0;
     }
 
+    //Moving carousel
     track.style.transform = `translateX(-${currentIndex * 100}%)`;
 }
 
+//Changes slide every 3 seconds
 setInterval(changeSlide, 3000);
--- Manhua.js
+++ Manhua.js
@@ -2,6 +2,8 @@ function toggleContent(button) {
     var container = button.closest(".books");
     var node = container.querySelector(".dropdown");
 
+
+    //Opens and closes extra content
     if (node.style.display === "none" ) {
         node.style.display = "block";
         button.style.transform = "rotate(180deg)";
@@ -12,6 +14,7 @@ function toggleContent(button) {
 }
 
 function searchContent() {
+    //Makes searched content into lowercase and removes extra whitespace at front and back
     const input = document
         .getElementById("search") .value .toLowerCase().trim();
 
@@ -20,6 +23,7 @@ function searchContent() {
 
     let titles = 0
 
+    //Comparing searched content to titles in the site
     books.forEach(function (bookTitle) {
         const title = bookTitle.textContent.toLowerCase();
         const bookContainer = bookTitle.closest(".books");
@@ -32,11 +36,12 @@ function searchContent() {
             bookContainer.style.display = "none";
         }
 
-    if (titles === 0){
-        noResults.style.display = "block"
-        }
-    else{
-        noResults.style.display = "none"
+        //Shown if no titles 
+        if (titles === 0){
+            noResults.style.display = "block"
+            }
+        else{
+            noResults.style.display = "none"
 
     }
     });
@@ -46,6 +51,7 @@ function currentPage(){
     const location = window.location.href; 
     const links = document.querySelectorAll(".nav-link")
 
+    //Makes current page selector darker
     links.forEach(function(link) {
         if (link.href === location){
             link.style.backgroundColor = "rgb(104, 34, 34)"
@@ -55,21 +61,24 @@ function currentPage(){
     })
     
 }
+//currentPage function loaded when page loads
 window.addEventListener("DOMContentLoaded", currentPage);    
 
 function filterTag(tag) {
     const books = document.querySelectorAll(".books");
     const title = document.querySelector(".category-shown")
 
+    //Filtering out books without selected tag
     books.forEach(function(book) {
         const bookTags = book.querySelector(".tags").textContent;
-
+    
         if (bookTags.includes(tag)) {
             book.style.display = "block";
         } else {
             book.style.display = "none";
         }
     });
+    //Changes archive title to tag name
      title.textContent = tag
 }
 
@@ -77,6 +86,7 @@ function showAll() {
     const books = document.querySelectorAll(".books");
     const title = document.querySelector(".category-shown")
 
+    //Removes any tag filtering
     books.forEach(function(book) {
         book.style.display = "block";
     });
